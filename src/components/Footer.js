47b5c0d22b39f4e97defe5139c20fc071c32d81a
Thelma-Dev <osifoaisosa@gmail.com>
2023-06-02 18:35:16 -0500

    Updated Social Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,9 @@ function Footer(props) {
                 </div>
                 <div className="social-icons">
                     <ul>
-                        <li><a href= "#">{props.facebookLogo}</a></li>
-                        <li><a href= "#">{props.instagramLogo}</a></li>
-                        <li><a href= "#">{props.twitterLogo}</a></li>
+                        <li><a href= "#!">{props.facebookLogo}</a></li>
+                        <li><a href= "#!">{props.instagramLogo}</a></li>
+                        <li><a href= "#!">{props.twitterLogo}</a></li>
                     </ul>   
                     <p className="copyright">Designed by {props.developer} &copy; {year} All rights reserved</p>
                 </div>
